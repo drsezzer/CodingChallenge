0095bbf964b8ac88f219d4ff54bdb7598de854d7
SMercer <smercer@turing.ac.uk>
2024-07-17 11:38:11 +0100

    Dame Judy, standard prompt, third attempt

--- personas/DameJudy_2.py
+++ personas/DameJudy_2.py
@@ -5,29 +5,27 @@ b 2 d 0
 b 3 b 2
 b 3 e 2
 c 4 d 0
-c 4 e 1"""
-
+c 4 e 1
+"""
 
 import sys
 from collections import defaultdict, deque
 
 def main():
-    # We'll read our inputs, the relations of dependencies.
     dependencies = defaultdict(list)
-    dependants = defaultdict(set)
-
-    # We must understand each line of input, interpreting the dependencies.
-    for line in input.split('\n'): # sys.stdin
+    direct_dependants = defaultdict(set)
+    
+    # Read input lines
+    for line in input.split('\n'): # sys.stdin:
         if line.strip():
             src_pkg, src_ver, dest_pkg, dest_ver = line.split()
             src = (src_pkg, int(src_ver))
             dest = (dest_pkg, int(dest_ver))
             dependencies[src].append(dest)
-            dependants[dest].add(src)
+            direct_dependants[dest].add(src)
 
-    # Now, we determine the reach of each package, both direct and by extension.
-    def count_transitive_dependents(package):
-        # Let's find out how many are indirectly dependent on our package.
+    # Function to find all transitive dependants
+    def find_transitive_dependants(package):
         visited = set()
         queue = deque([package])
         while queue:
@@ -38,25 +36,22 @@ def main():
                     queue.append(dependent)
         return visited
 
-    # We seek the character with the deepest connections.
+    # Determine the most problematic package
     most_problematic = None
-    max_ratio = -1
+    highest_ratio = -1
+    
+    # Compute for each package that has at least one direct dependent
+    for package in direct_dependants:
+        transitive_dependants = find_transitive_dependants(package)
+        direct_deps = direct_dependants[package]
+        ratio = len(transitive_dependants) / len(direct_deps)
+        print(f"Package: {package}, Ratio: {ratio}, Transitive: {len(transitive_dependants)}, Direct: {len(direct_deps)}")  # Debug output
+        if ratio > highest_ratio:
+            highest_ratio = ratio
+            most_problematic = package
 
-    for package in dependants:
-        # Direct dependents are those immediately reliant on our package.
-        direct_dependents = dependants[package]
-        # Transitive dependents include the full range of influence.
-        all_dependents = count_transitive_dependents(package)
-        
-        if direct_dependents:
-            ratio = len(all_dependents) / len(direct_dependents)
-            if ratio > max_ratio:
-                max_ratio = ratio
-                most_problematic = package
-
-    # We'll announce the one with the heaviest burden to carry.
     if most_problematic:
-        print(f"{most_problematic[0]} {most_problematic[1]}")
+        print(f"Most problematic package: {most_problematic[0]} {most_problematic[1]}")
 
 if __name__ == "__main__":
     main()
