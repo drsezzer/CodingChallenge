0e7fe2991f5b24426aaebc73d932d3fa89257748
SMercer <smercer@turing.ac.uk>
2024-07-18 12:59:40 +0100

    exceptions again

--- personas/BigBang-HowardWolowitz.py
+++ personas/BigBang-HowardWolowitz.py
@@ -20,27 +20,25 @@ import sys
 from collections import defaultdict, deque
 
 def read_dependencies():
-    # Reading input and building the graph
     dependencies = defaultdict(list)
     dependants = defaultdict(set)
 
-    try:
-        for line in input.splitlines():
-            if line.strip():
-                pkg1, ver1, pkg2, ver2 = line.split()
-                ver1, ver2 = int(ver1), int(ver2)
-                dependencies[(pkg1, ver1)].append((pkg2, ver2))
-                dependants[(pkg2, ver2)].add((pkg1, ver1))
-    except Exception as e:
-        print("Error reading input:", e)
-        sys.exit(1)
+    # Reading input from standard input
+    input_data = input.splitlines()
+    for line in input_data:
+        if line.strip():
+            pkg1, ver1, pkg2, ver2 = line.split()
+            ver1, ver2 = int(ver1), int(ver2)
+            # pkg1 depends on pkg2
+            dependencies[(pkg1, ver1)].append((pkg2, ver2))
+            # pkg2 is depended on by pkg1
+            dependants[(pkg2, ver2)].add((pkg1, ver1))
 
     return dependencies, dependants
 
 def calculate_transitive_dependants(dependants):
-    # Calculates all transitive dependants using a BFS approach
     all_dependants = {}
-    for package in list(dependants.keys()):  # Work with a list of keys to avoid dictionary change during iteration
+    for package in dependants:
         visited = set()
         queue = deque([package])
         while queue:
@@ -53,26 +51,25 @@ def calculate_transitive_dependants(dependants):
         all_dependants[package] = visited - {package}  # Remove the package itself from its dependants
     return all_dependants
 
-def find_most_problematic(dependencies, all_dependants):
+def find_most_problematic(dependants, all_dependants):
     max_ratio = -1
     problematic_package = None
 
-    for package in list(dependencies.keys()):  # Similarly, use list to avoid runtime dictionary change issues
-        if package in all_dependants:
-            transitive_count = len(all_dependants[package])
-            direct_count = len(dependencies[package])
-            if direct_count > 0:  # Avoid division by zero
-                ratio = transitive_count / direct_count
-                if ratio > max_ratio:
-                    max_ratio = ratio
-                    problematic_package = package
+    for package, transitive_deps in all_dependants.items():
+        transitive_count = len(transitive_deps)
+        direct_count = len(dependants[package])  # Direct dependants
+        if direct_count > 0:  # To avoid division by zero
+            ratio = transitive_count / direct_count
+            if ratio > max_ratio:
+                max_ratio = ratio
+                problematic_package = package
 
     return problematic_package
 
 def main():
     dependencies, dependants = read_dependencies()
     all_dependants = calculate_transitive_dependants(dependants)
-    problematic_package = find_most_problematic(dependencies, all_dependants)
+    problematic_package = find_most_problematic(dependants, all_dependants)
     
     if problematic_package:
         print(f"{problematic_package[0]} {problematic_package[1]}")
