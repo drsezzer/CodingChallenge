3523664cb76b09777b2e5409114b56b73e75a856
SMercer <smercer@turing.ac.uk>
2024-07-18 10:24:10 +0100

    stil wrong answer

--- personas/StarTrek-CmdrData.py
+++ personas/StarTrek-CmdrData.py
@@ -11,7 +11,7 @@ c 4 d 0
 c 4 e 1"""
 
 import sys
-from collections import defaultdict, deque
+from collections import defaultdict
 
 def read_input():
     dependencies = []
@@ -23,43 +23,42 @@ def read_input():
 
 def build_graph(dependencies):
     graph = defaultdict(list)
-    reverse_graph = defaultdict(list)
     for depender, depender_ver, dependee, dependee_ver in dependencies:
-        reverse_graph[(dependee, dependee_ver)].append((depender, depender_ver))
-    return graph, reverse_graph
+        graph[(dependee, dependee_ver)].append((depender, depender_ver))
+    return graph
 
-def calculate_dependents(graph):
-    direct_count = defaultdict(int)
-    transitive_sets = defaultdict(set)
+def compute_dependents(graph):
+    direct = defaultdict(set)
+    all_dependents = defaultdict(set)
 
-    # First collect direct dependants
-    for node, dependants in graph.items():
-        for dependant in dependants:
-            direct_count[node] += 1
-            transitive_sets[dependant].add(node)
+    # Collect direct dependents
+    for node, dependents in graph.items():
+        for dependent in dependents:
+            direct[node].add(dependent)
+            all_dependents[node].add(dependent)
 
-    # Calculate transitive dependants using DFS to avoid modification during traversal
+    # Expand to transitive dependents using a recursive DFS
     def dfs(node, visited):
-        for neighbour in graph[node]:
-            if neighbour not in visited:
-                visited.add(neighbour)
-                transitive_sets[neighbour].update(transitive_sets[node])
-                dfs(neighbour, visited)
+        for dependent in direct[node]:
+            if dependent not in visited:
+                visited.add(dependent)
+                all_dependents[dependent].update(all_dependents[node])
+                dfs(dependent, visited)
 
-    nodes = list(graph.keys())  # List of all nodes to avoid dict size change error
-    for node in nodes:
-        dfs(node, set([node]))
+    for node in graph:
+        dfs(node, set())
 
-    transitive_count = {node: len(dependents) for node, dependents in transitive_sets.items()}
-    return transitive_count, direct_count
+    transitive = {node: len(all_deps - direct_deps) for node, all_deps in all_dependents.items() for direct_deps in (direct[node],)}
+    direct_count = {node: len(direct_deps) for node, direct_deps in direct.items()}
 
-def find_most_problematic(transitive_count, direct_count):
+    return transitive, direct_count
+
+def find_most_problematic(transitive, direct):
     max_ratio = -1
     problematic_package = None
-    for node in direct_count:
-        if direct_count[node] > 0:  # Ensure we have direct dependants to avoid division by zero
-            trans = transitive_count.get(node, 0)
-            ratio = trans / direct_count[node]
+    for node in direct:
+        if direct[node] > 0:  # Ensure we have direct dependants
+            ratio = transitive[node] / direct[node]
             if ratio > max_ratio:
                 max_ratio = ratio
                 problematic_package = node
@@ -67,9 +66,9 @@ def find_most_problematic(transitive_count, direct_count):
 
 def main():
     dependencies = read_input()
-    _, reverse_graph = build_graph(dependencies)
-    transitive_count, direct_count = calculate_dependents(reverse_graph)
-    problematic_package = find_most_problematic(transitive_count, direct_count)
+    graph = build_graph(dependencies)
+    transitive, direct = compute_dependents(graph)
+    problematic_package = find_most_problematic(transitive, direct)
     if problematic_package:
         print(f"{problematic_package[0]} {problematic_package[1]}")
     else:
